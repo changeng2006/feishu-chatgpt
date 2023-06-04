37775f743ec5c237cbfab83c00c54f66da885fe8
changeng2006 <73058086+changeng2006@users.noreply.github.com>
2023-06-04 16:19:46 +0800

    Update cache.ts

--- lib/cache.ts
+++ lib/cache.ts
@@ -2,9 +2,9 @@ import LRU from 'lru-cache'
 
 // TODO: serverless 对 cache 不太友好
 export const cache = new LRU({
-  max: 10000,
-  maxSize: 100000,
-  ttl: 5000 * 120 * 120 * 4,
+  max: 1000,
+  maxSize: 50000,
+  ttl: 1000 * 60 * 60 * 2,
   sizeCalculation(key, value) {
     return typeof value === 'string' ? value.length : 1
   },
